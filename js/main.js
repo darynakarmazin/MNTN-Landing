08506fe5f5a5f7b40845583af5d91e7c5c13609f
Daryna Karmazin <daryna.karmazin@gmail.com>
2025-02-08 19:02:46 +0200

    Added IntersectionObserver js script

--- js/main.js
+++ js/main.js
@@ -4,3 +4,34 @@ const menu = document.querySelector(".menu__list");
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("menu__list--open");
 });
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const activeId = entry.target.id;
+
+        document.querySelectorAll(".site-list__item").forEach((item) => {
+          item.classList.remove("site-list__item--active");
+        });
+
+        const activeLink = document.querySelector(
+          `.site-list__link[href="#${activeId}`
+        );
+
+        if (activeLink) {
+          activeLink
+            .closest(".site-list__item")
+            .classList.add("site-list__item--active");
+        }
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+document
+  .querySelectorAll("#slide-0, #slide-1, #slide-2, #slide-3")
+  .forEach((element) => {
+    observer.observe(element);
+  });
